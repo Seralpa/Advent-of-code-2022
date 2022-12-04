5c1ffe9cd73d300d114789866c0ea2a39d0f10e9
seralpa <seralpa789@gmail.com>
2022-12-04 07:07:06 +0100

    day 4 done

---
+++ day4/p1.py
@@ -0,0 +1,13 @@
+import os
+
+cwd = os.path.dirname(os.path.abspath(__file__))
+with open(f"{cwd}/input.txt") as f:
+	pair_assignments = [map(int, l.split(',')) for l in f.read().replace('-', ',').splitlines()]
+
+count = 0
+for a1_start, a1_end, a2_start, a2_end in pair_assignments:
+	r1 = set(range(a1_start, a1_end + 1))
+	r2 = set(range(a2_start, a2_end + 1))
+	if r1.issubset(r2) or r2.issubset(r1):
+		count += 1
+print(count)
