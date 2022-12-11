d26d224f20a56003cf57a6aa1944c6b2ce6c4812
seralpa <seralpa789@gmail.com>
2022-12-11 09:07:26 +0100

    day 11 done

---
+++ day11/p2.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+from dataclasses import dataclass
+import os
+import math
+
+
+@dataclass
+class Monkey:
+	items: list[int]
+	op: str
+	test: int
+	next_id: tuple[int, int]
+	inspected: int = 0
+
+	def process_items(self, monkeys: list[Monkey], divisor: int):
+		for i in self.items:
+			self.inspected += 1
+			i = eval(self.op) % divisor
+			next_id = self.next_id[0] if i % self.test == 0 else self.next_id[1]
+			monkeys[next_id].items.append(i)
+		self.items = []
+
+
+cwd = os.path.dirname(os.path.abspath(__file__))
+with open(f"{cwd}/input.txt") as f:
+	data = f.read().replace("Monkey ", "")
+	data = data.replace("  Starting items: ", "")
+	data = data.replace("  Operation: new = ", "")
+	data = data.replace("  Test: divisible by ", "")
+	data = data.replace("    If true: throw to monkey ", "")
+	data = data.replace("    If false: throw to monkey ", "")
+	data = data.replace(":", "").replace("old", "i")
+	data = [l.splitlines() for l in data.split("\n\n")]
+
+monkeys: list[Monkey] = []
+divisor = 1
+for m in data:
+	items = list(map(int, m[1].split(", ")))
+	divisor *= int(m[3])
+	monkeys.append(Monkey(items, op=m[2], test=int(m[3]), next_id=(int(m[4]), int(m[5]))))
+
+for round in range(10000):
+	for m in monkeys:
+		m.process_items(monkeys, divisor)
+
+print(math.prod(sorted([m.inspected for m in monkeys])[-2:]))
