50c85c917ad462c20d27c08575133631ea5bbdce
seralpa <seralpa789@gmail.com>
2022-12-25 10:07:28 +0100

    day 25 done

--- day25/p1.py
+++ day25/p1.py
@@ -12,17 +12,12 @@ for num in data:
 	count += sum([(convert[c] if c in "-=" else int(c)) * 5**i for i, c in enumerate(reversed(num))])
 
 count = list(reversed(str(np.base_repr(count, base=5))))
-overflow = False
+dict_snafu = {"3": "=", "4": "-", "5": "0"}
 for i, c in enumerate(count):
 	if c in "012":
 		continue
-	if c == "3":
-		count[i] = "="
-	elif c == "4":
-		count[i] = "-"
-	elif c == "5":
-		count[i] = "0"
-	if i == 0:
+	count[i] = dict_snafu[c]
+	if i == len(count) - 1:
 		count.append("0")
 	count[i + 1] = str(int(count[i + 1]) + 1)
 print("".join(reversed(count)))
