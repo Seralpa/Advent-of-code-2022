50ac40f6c31a898f9d95e101cc3e08aa4d86779d
seralpa <seralpa789@gmail.com>
2023-04-05 07:24:23 +0200

    refactored to improve efficiency by using ranges and intersections

--- day15/p1.py
+++ day15/p1.py
@@ -1,20 +1,24 @@
 import os
 
+Point = tuple[int, int]
+
 
 class Sensor:
-	pos: tuple[int, int]
-	rang: int
+	x: int
+	y: int
+	range_: int
 
-	def __init__(self, sens: tuple[int, int], beacon: tuple[int, int]):
-		self.pos = sens
-		self.rang = abs(sens[0] - beacon[0]) + abs(sens[1] - beacon[1])
+	def __init__(self, pos: Point, beacon: Point):
+		self.x = pos[0]
+		self.y = pos[1]
+		self.range_ = abs(pos[0] - beacon[0]) + abs(pos[1] - beacon[1])
 
-	def get_distance(self, pos: tuple[int, int]) -> int:
-		return abs(self.pos[0] - pos[0]) + abs(self.pos[1] - pos[1])
+	def get_distance(self, pos: Point) -> int:
+		return abs(self.x - pos[0]) + abs(self.y - pos[1])
 
-	def get_pos_in_range_y(self, y: int) -> set[tuple[int, int]]:
-		dist = self.rang - self.get_distance((self.pos[0], y))
-		return set([(x, y) for x in range(self.pos[0] - dist, self.pos[0] + dist)])
+	def get_covered_range_in_y(self, y: int) -> Point:
+		dist = self.range_ - self.get_distance((self.x, y))
+		return (self.x - dist, self.x + dist) if dist >= 0 else (0, 0)
 
 
 cwd = os.path.dirname(os.path.abspath(__file__))
@@ -22,7 +26,11 @@ with open(f"{cwd}/input.txt") as f:
 	sensors = f.read().replace("Sensor at x=", "").replace(" closest beacon is at x=", "")
 	sensors = [Sensor(*[tuple(map(int, t.split(", y="))) for t in l.split(":")]) for l in sensors.splitlines()]
 
-covered_pos = set()
-for s in sensors:
-	covered_pos |= s.get_pos_in_range_y(2000000)
-print(len(covered_pos))
+ranges = sorted([s.get_covered_range_in_y(2000000) for s in sensors])
+for i in range(len(ranges) - 1):
+	r1, r2 = ranges[i], ranges[i + 1]
+	if max(r1[0], r2[0]) <= min(r1[1], r2[1]):
+		ranges[i] = (0, 0)
+		ranges[i + 1] = (min(r1[0], r2[0]), max(r1[1], r2[1]))
+
+print(sum(r[1] - r[0] for r in ranges))
--- day15/p2.py
+++ day15/p2.py
@@ -2,47 +2,56 @@ import os
 from typing import Generator
 
 MAX_SIZE = 4000000
+Point = tuple[int, int]
 
 
 class Sensor:
-	pos: tuple[int, int]
-	rang: int
+	x: int
+	y: int
+	range_: int
 
-	def __init__(self, sens: tuple[int, int], beacon: tuple[int, int]):
-		self.pos = sens
-		self.rang = abs(sens[0] - beacon[0]) + abs(sens[1] - beacon[1])
+	def __init__(self, pos: Point, beacon: Point):
+		self.x = pos[0]
+		self.y = pos[1]
+		self.range_ = abs(pos[0] - beacon[0]) + abs(pos[1] - beacon[1])
 
-	def get_border(self) -> Generator[tuple[int, int], None, None]:
-		x, y = self.pos[0] - self.rang - 1, self.pos[1]
-		for _ in range(self.rang + 1):
-			if x >= 0 and x <= MAX_SIZE and y >= 0 and y <= MAX_SIZE:
-				yield x, y
-			x += 1
-			y += 1
-		x, y = self.pos[0], self.pos[1] + self.rang + 1
-		for _ in range(self.rang + 1):
-			if x >= 0 and x <= MAX_SIZE and y >= 0 and y <= MAX_SIZE:
-				yield x, y
-			x += 1
-			y -= 1
-		x, y = self.pos[0] + self.rang + 1, self.pos[1]
-		for _ in range(self.rang + 1):
-			if x >= 0 and x <= MAX_SIZE and y >= 0 and y <= MAX_SIZE:
-				yield x, y
-			x -= 1
-			y -= 1
-		x, y = self.pos[0], self.pos[1] - self.rang - 1
-		for _ in range(self.rang + 1):
-			if x >= 0 and x <= MAX_SIZE and y >= 0 and y <= MAX_SIZE:
-				yield x, y
-			x -= 1
-			y += 1
+	def get_border_ranges(self) -> list[tuple[Point, Point]]:
+		n = self.x - self.range_ - 1, self.y
+		s = self.x + self.range_ + 1, self.y
+		w = self.x, self.y - self.range_ - 1
+		e = self.x, self.y + self.range_ + 1
+		return [(n, w), (n, e), (w, s), (e, s)]
 
-	def pos_in_range(self, pos: tuple[int, int]):
-		return self.get_distance(pos) <= self.rang
+	def remove_range_overlap(self, r: tuple[Point, Point]) -> tuple[Point, Point]:
+		# NOTE the case when the scanner overlaps only the middle of the range is not handled
+		# as in the end all ranges will be reduced to a single point
+		p1_in_range = self.pos_in_range(r[0])
+		p2_in_range = self.pos_in_range(r[1])
+		if p1_in_range and p2_in_range:
+			return (0, 0), (0, 0)
+		# dir_x will always be 1 because of how the ranges are constructed initially
+		dir_y = 1 if r[1][1] >= r[0][1] else -1
+		if p1_in_range:
+			d_x, d_y = self.get_distance_xy(r[0])
+			d = d_x + d_y * dir_y
+			delta = ((self.range_ - d) // 2) + 1
+			return (r[0][0] + delta, r[0][1] + delta * dir_y), r[1]
+		elif p2_in_range:
+			d_x, d_y = self.get_distance_xy(r[1])
+			d = -d_x - d_y * dir_y
+			delta = (self.range_ - d + 1) // 2
+			return r[0], (r[1][0] - delta, r[1][1] - delta * dir_y)
+		else:
+			return r
 
-	def get_distance(self, pos: tuple[int, int]) -> int:
-		return abs(self.pos[0] - pos[0]) + abs(self.pos[1] - pos[1])
+	def pos_in_range(self, pos: Point):
+		return self.get_distance(pos) <= self.range_
+
+	def get_distance(self, pos: Point) -> int:
+		return abs(self.x - pos[0]) + abs(self.y - pos[1])
+
+	def get_distance_xy(self, pos: Point) -> tuple[int, int]:
+		return pos[0] - self.x, pos[1] - self.y
 
 
 cwd = os.path.dirname(os.path.abspath(__file__))
@@ -50,14 +59,41 @@ with open(f"{cwd}/input.txt") as f:
 	sensors = f.read().replace("Sensor at x=", "").replace(" closest beacon is at x=", "")
 	sensors = [Sensor(*[tuple(map(int, t.split(", y="))) for t in l.split(":")]) for l in sensors.splitlines()]
 
-for i, s in enumerate(sensors):
-	print(f"border of sensor {i}/{len(sensors)}")
-	for p in s.get_border():
-		free = True
-		for s1 in sensors:
-			if s1.pos_in_range(p):
-				free = False
-				break
-		if free:
-			print(p[0] * 4000000 + p[1])
-			exit()
+ranges = []
+for s in sensors:
+	ranges += s.get_border_ranges()
+
+for i in range(len(ranges)):
+	if ranges[i][0][0] > MAX_SIZE or ranges[i][1][0] < 0:
+		ranges[i] = ((0, 0), (0, 0))
+		continue
+	dir_y = 1 if ranges[i][1][1] >= ranges[i][0][1] else -1
+	if ranges[i][0][0] < 0:
+		ranges[i] = (0, ranges[i][0][1] - ranges[i][0][0] * dir_y), ranges[i][1]
+	if ranges[i][1][0] > MAX_SIZE:
+		ranges[i] = ranges[i][0], (MAX_SIZE, ranges[i][1][1] - (ranges[i][1][0] - MAX_SIZE) * dir_y)
+
+	if dir_y == 1:
+		if ranges[i][1][1] < 0 or ranges[i][0][1] > MAX_SIZE:
+			ranges[i] = ((0, 0), (0, 0))
+			continue
+		if ranges[i][0][1] < 0:
+			ranges[i] = (ranges[i][0][0] - ranges[i][0][1], 0), ranges[i][1]
+		if ranges[i][1][1] > MAX_SIZE:
+			ranges[i] = ranges[i][0], (ranges[i][1][0] - (ranges[i][1][1] - MAX_SIZE), MAX_SIZE)
+	else:
+		if ranges[i][0][1] < 0 or ranges[i][1][1] > MAX_SIZE:
+			ranges[i] = ((0, 0), (0, 0))
+			continue
+		if ranges[i][1][1] < 0:
+			ranges[i] = ranges[i][0], (ranges[i][1][0] + ranges[i][1][1], 0)
+		if ranges[i][0][1] > MAX_SIZE:
+			ranges[i] = (ranges[i][0][0] + (ranges[i][0][1] - MAX_SIZE), MAX_SIZE), ranges[i][1]
+
+while len(ranges) > 1 or ranges[0][0] != ranges[0][1]:
+	for s in sensors:
+		for i, r in enumerate(ranges):
+			ranges[i] = s.remove_range_overlap(r)
+	ranges = list(set([r for r in ranges if r != ((0, 0), (0, 0))]))
+
+print(ranges[0][0][0] * 4000000 + ranges[0][0][1])
